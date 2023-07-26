af2d9ec34272acc463fd7e8d1bdd427a2b9c0804
Theo Arnault <28763293+ibhuu@users.noreply.github.com>
2023-07-26 12:07:35 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <!-- NOTE: Remove this anchor when uncommenting unocss-placeholder -->
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="h-[6rem] p-5.5" alt="Svelte Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="h-6rem p-5" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p>
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<!-- <style>
+  @unocss-placeholder
+</style> -->
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import UnoCSS from 'unocss/vite'
+import extractorSvelte from '@unocss/extractor-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+
+    UnoCSS({
+
+      mode: 'shadow-dom',
+      extractors: [extractorSvelte()]
+    }),
+    svelte()
+  ],
+})
